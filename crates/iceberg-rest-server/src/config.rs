3291d9ab57fd8c44aad130182b4dc342ba226152
Tobias Puetz <tobias@hansetag.com>
2024-06-07 12:58:14 +0200

    move to clap to get rid of yaml-rust & add audit.toml to ignore the false positive in mysql

--- crates/iceberg-rest-server/src/config.rs
+++ crates/iceberg-rest-server/src/config.rs
@@ -1,18 +1,24 @@
 //! Contains Configuration of the SAGA Module
-use config::Config;
+use clap::Parser;
 use std::collections::HashSet;
+use std::convert::Infallible;
+use std::ops::Deref;
+use std::str::FromStr;
 
 use crate::WarehouseIdent;
 
-const DEFAULT_RESERVED_NAMESPACES: [&str; 2] = ["system", "examples"];
-
-#[derive(Debug, Clone, serde::Deserialize, PartialEq)]
+#[derive(Debug, Clone, serde::Deserialize, PartialEq, Parser)]
 #[allow(clippy::module_name_repetitions)]
 /// Configuration of this Module
 pub struct DynAppConfig {
     /// Base URL for this REST Catalog.
     /// This is used as the "uri" and "s3.signer.url"
     /// while generating the Catalog Config
+    #[clap(
+        long,
+        env = "ICEBERG_REST__BASE_URI",
+        default_value = "https://localhost:8080/catalog/"
+    )]
     pub base_uri: url::Url,
     /// Template to obtain the "prefix" for a warehouse,
     /// may contain `{warehouse_id}` placeholder.
@@ -21,18 +27,72 @@ pub struct DynAppConfig {
     /// `warehouse_id`, make sure to strip them using a
     /// reverse proxy before routing to the catalog service.
     /// Example value: `{warehouse_id}`
+    #[clap(
+        long,
+        env = "ICEBERG_REST__PREFIX_TEMPLATE",
+        default_value = "{warehouse_id}"
+    )]
     prefix_template: String,
     /// Reserved namespaces that cannot be created by users.
     /// This is used to prevent users to create certain
     /// (sub)-namespaces. By default, `system` and `examples` are
     /// reserved. More namespaces can be added here.
-    pub reserved_namespaces: HashSet<String>,
+    #[clap(
+        long,
+        env = "ICEBERG_REST__RESERVED_NAMESPACES",
+        default_value = "system,examples"
+    )]
+    pub reserved_namespaces: ReservedNamespaces,
     // ------------- POSTGRES IMPLEMENTATION -------------
+    #[clap(
+        long,
+        env = "ICEBERG_REST__PG_ENCRYPTION_KEY",
+        default_value = "<This is unsafe, please set a proper key>"
+    )]
     pub(crate) pg_encryption_key: String,
+    #[clap(
+        long,
+        env = "ICEBERG_REST__PG_DATABASE_URL_READ",
+        default_value = "postgres://postgres:password@localhost:5432/iceberg"
+    )]
     pub(crate) pg_database_url_read: String,
+    #[clap(
+        long,
+        env = "ICEBERG_REST__PG_DATABASE_URL_WRITE",
+        default_value = "postgres://postgres:password@localhost:5432/iceberg"
+    )]
     pub(crate) pg_database_url_write: String,
+    #[clap(
+        long,
+        env = "ICEBERG_REST__PG_READ_POOL_CONNECTIONS",
+        default_value = "10"
+    )]
     pub pg_read_pool_connections: u32,
+    #[clap(
+        long,
+        env = "ICEBERG_REST__PG_WRITE_POOL_CONNECTIONS",
+        default_value = "5"
+    )]
     pub pg_write_pool_connections: u32,
+}
+#[derive(Debug, Clone, serde::Deserialize, PartialEq)]
+pub struct ReservedNamespaces(HashSet<String>);
+impl Deref for ReservedNamespaces {
+    type Target = HashSet<String>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl FromStr for ReservedNamespaces {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(ReservedNamespaces(
+            s.split(',').map(str::to_string).collect(),
+        ))
+    }
 }
 
 impl DynAppConfig {
@@ -61,66 +121,11 @@ impl DynAppConfig {
     }
 }
 
-fn build_config() -> DynAppConfig {
-    let mut config: DynAppConfig = Config::builder()
-        // ToDo: base_uri requires trailing slash. Make it work without it.
-        .set_default("base_uri", "https://localhost:8080/catalog/")
-        .expect("Valid base_url")
-        .set_default("prefix_template", "{warehouse_id}")
-        .expect("Valid prefix_template")
-        .set_default("reserved_namespaces", Vec::<String>::default())
-        .expect("Valid reserved_namespaces")
-        .set_default(
-            "pg_encryption_key",
-            "<This is unsafe, please set a proper key>",
-        )
-        .expect("Valid pg_encryption_key")
-        .set_default(
-            "pg_database_url_read",
-            "postgres://postgres:password@localhost:5432/iceberg",
-        )
-        .expect("Valid pg_database_url")
-        .set_default(
-            "pg_database_url_write",
-            "postgres://postgres:password@localhost:5432/iceberg",
-        )
-        .expect("Valid pg_database_url")
-        .set_default("pg_read_pool_connections", 10)
-        .expect("Valid pg_read_pool_connections")
-        .set_default("pg_write_pool_connections", 5)
-        .expect("Valid pg_write_pool_connections")
-        .add_source(
-            config::Environment::with_prefix("ICEBERG_REST")
-                .try_parsing(true)
-                .separator("__")
-                .with_list_parse_key("RESERVED_NAMESPACES")
-                .list_separator(","),
-        )
-        .build()
-        .expect("Cannot build 'DynAppConfig'.")
-        .try_deserialize()
-        .expect("Cannot deserialize 'DynAppConfig'.");
-
-    config.reserved_namespaces = config
-        .reserved_namespaces
-        .into_iter()
-        .map(|namespace| namespace.to_lowercase())
-        .chain(
-            DEFAULT_RESERVED_NAMESPACES
-                .into_iter()
-                .map(ToOwned::to_owned)
-                .collect::<Vec<String>>(),
-        )
-        .collect::<HashSet<String>>();
-
-    config
-}
-
 lazy_static::lazy_static! {
     #[derive(Debug)]
     /// Configuration of the SAGA Module
     pub static ref CONFIG: DynAppConfig = {
-        build_config()
+        DynAppConfig::parse()
     };
 }
 
