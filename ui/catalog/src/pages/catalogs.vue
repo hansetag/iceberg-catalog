0898312ba878d58aee71e69c8fc3f4b35b61926b
Viktor <vikkes@gmail.com>
2024-07-19 16:48:49 +0200

    catalog fetch

--- ui/catalog/src/pages/catalogs.vue
+++ ui/catalog/src/pages/catalogs.vue
@@ -1,7 +1,20 @@
 <template>
   <div>Catalogs</div>
+  {{ data }}
 </template>
 
 <script lang="ts" setup>
 //
+
+const url = "http://localhost:8080/management/v1/";
+
+const data = ref(null);
+const error = ref(null);
+
+onMounted(() => {
+  fetch(url + "/project")
+    .then((res) => res.json())
+    .then((json) => (data.value = json))
+    .catch((err) => (error.value = err));
+});
 </script>
