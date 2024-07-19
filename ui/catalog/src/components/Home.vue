3bddab297369712ef08f27e594ea1db7a5b847c6
Viktor <vikkes@gmail.com>
2024-07-19 07:08:57 +0200

    home page

--- ui/catalog/src/components/Home.vue
+++ ui/catalog/src/components/Home.vue
@@ -1,14 +1,14 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-centerfill-height mx-auto" max-width="900">
-      <v-img class="mb-4" height="150" src="@/assets/iceberg.png" />
-
       <div class="text-center">
+        <v-img class="mb-4" height="100" src="@/assets/bear.png" />
+
         <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
 
         <h1 class="text-h2 font-weight-bold">TIP</h1>
-        <v-img class="mb-4" height="100" src="@/assets/bear.png" />
       </div>
+      <v-img class="mb-4" height="150" src="@/assets/iceberg.png" />
 
       <div class="py-4" />
     </v-responsive>
