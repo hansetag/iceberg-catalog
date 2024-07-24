2c4881a7283da20dcdabac95747de9c16cc7643c
Tobias Puetz <tobias@hansetag.com>
2024-07-24 14:15:17 +0200

    reorg cli into modules

--- crates/iceberg-catalog-bin/src/main.rs
+++ crates/iceberg-catalog-bin/src/main.rs
@@ -1,3 +1,5 @@
+mod serve;
+
 use anyhow::{Context, Error};
 use clap::{Parser, Subcommand};
 use iceberg_catalog::api::management::v1::ManagementApiDoc;
@@ -25,6 +27,9 @@ use reqwest::Url;
 use std::sync::Arc;
 use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::EnvFilter;
+
+mod healthcheck;
+mod wait_for_db;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -97,94 +102,6 @@ enum Commands {
     ManagementOpenapi {},
 }
 
-async fn serve(bind_addr: std::net::SocketAddr) -> Result<(), anyhow::Error> {
-    let read_pool =
-        iceberg_catalog::implementations::postgres::get_reader_pool(CONFIG.to_pool_opts()).await?;
-    let write_pool =
-        iceberg_catalog::implementations::postgres::get_writer_pool(CONFIG.to_pool_opts()).await?;
-
-    let catalog_state = CatalogState::from_pools(read_pool.clone(), write_pool.clone());
-    let secrets_state = SecretsState::from_pools(read_pool, write_pool);
-    let auth_state = AllowAllAuthState;
-
-    let health_provider = ServiceHealthProvider::new(
-        vec![
-            ("catalog", Arc::new(catalog_state.clone())),
-            ("secrets", Arc::new(secrets_state.clone())),
-            ("auth", Arc::new(auth_state.clone())),
-        ],
-        CONFIG.health_check_frequency_seconds,
-        CONFIG.health_check_jitter_millis,
-    );
-    health_provider.spawn_health_checks().await;
-
-    let mut cloud_event_sinks = vec![];
-
-    if let Some(nat_addr) = &CONFIG.nats_address {
-        let nats_publisher = build_nats_client(nat_addr).await?;
-        cloud_event_sinks
-            .push(Arc::new(nats_publisher) as Arc<dyn CloudEventBackend + Sync + Send>);
-    } else {
-        tracing::info!("Running without publisher.");
-    };
-
-    // TODO: what about this magic number
-    let (tx, rx) = tokio::sync::mpsc::channel(1000);
-
-    let x = CloudEventsPublisherBackgroundTask {
-        source: rx,
-        sinks: cloud_event_sinks,
-    };
-    let listener = tokio::net::TcpListener::bind(bind_addr).await?;
-
-    let (metrics_layer, metrics_future) =
-        iceberg_catalog::metrics::get_axum_layer_and_install_recorder(CONFIG.metrics_port)?;
-
-    let router = new_full_router::<
-        Catalog,
-        Catalog,
-        AllowAllAuthZHandler,
-        AllowAllAuthZHandler,
-        SecretsStore,
-    >(
-        auth_state,
-        catalog_state,
-        secrets_state,
-        CloudEventsPublisher::new(tx.clone()),
-        ContractVerifiers::new(vec![]),
-        if let Some(uri) = CONFIG.openid_provider_uri.clone() {
-            Some(Verifier::new(uri).await?)
-        } else {
-            None
-        },
-        health_provider,
-        Some(metrics_layer),
-    );
-
-    let publisher_handle = tokio::task::spawn(async move {
-        match x.publish().await {
-            Ok(_) => tracing::info!("Exiting publisher task"),
-            Err(e) => tracing::error!("Publisher task failed: {e}"),
-        };
-    });
-
-    tokio::task::spawn(async move {
-        tracing::info!(
-            "Starting metrics server listening on 0.0.0.0:{} ...",
-            CONFIG.metrics_port
-        );
-        metrics_future.await
-    });
-
-    service_serve(listener, router).await?;
-
-    tracing::debug!("Sending shutdown signal to event publisher.");
-    tx.send(Message::Shutdown).await?;
-    publisher_handle.await?;
-
-    Ok(())
-}
-
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
@@ -209,60 +126,9 @@ async fn main() -> anyhow::Result<()> {
             retries,
             backoff,
         }) => {
-            let mut counter = 0;
             let check_db = check_db || check_migrations;
-            if check_db {
-                loop {
-                    let Err(details) = db_health_check().await else {
-                        tracing::info!("Database is healthy.");
-                        break;
-                    };
-                    tracing::info!(?details,
-                        "DB not up yet, sleeping for {backoff}s before next retry. Retry: {counter}/{retries}",
-                    );
-                    counter += 1;
-                    if counter == retries {
-                        tracing::error!("Ran out of retries while waiting for db to come up.");
-                        anyhow::bail!("Ran out of retries while waiting for db to come up.");
-                    }
-                    tokio::time::sleep(std::time::Duration::from_secs(backoff)).await;
-                }
-            }
 
-            if check_migrations {
-                let mut counter = 0;
-                loop {
-                    let opts = CONFIG
-                        .to_pool_opts()
-                        .acquire_timeout(std::time::Duration::from_secs(1));
-
-                    let read_pool = get_reader_pool(opts).await?;
-                    let migrations =
-                        iceberg_catalog::implementations::postgres::check_migration_status(
-                            &read_pool,
-                        )
-                        .await;
-                    match migrations {
-                        Ok(MigrationState::Complete) => {
-                            tracing::info!("Database is up to date with binary.");
-                            break;
-                        }
-                        unready => {
-                            tracing::info!(?unready, "Database is not up to date with binary.");
-                        }
-                    }
-
-                    counter += 1;
-                    if counter > retries {
-                        tracing::error!("Ran out of retries while waiting for migrations.");
-                        anyhow::bail!("Ran out of retries while waiting for migrations.");
-                    }
-                    tracing::info!(
-                        "DB not up to date with binary yet, sleeping for {backoff}s before next retry. Retry: {counter}/{retries}",
-                    );
-                    tokio::time::sleep(std::time::Duration::from_secs(backoff)).await;
-                }
-            }
+            wait_for_db::wait_for_db(check_migrations, retries, backoff, check_db).await?;
         }
         Some(Commands::Migrate {}) => {
             print_info();
@@ -284,7 +150,7 @@ async fn main() -> anyhow::Result<()> {
             print_info();
             tracing::info!("Starting server on 0.0.0.0:{}...", CONFIG.listen_port);
             let bind_addr = std::net::SocketAddr::from(([0, 0, 0, 0], CONFIG.listen_port));
-            serve(bind_addr).await?;
+            serve::serve(bind_addr).await?;
         }
         Some(Commands::Healthcheck {
             check_all,
@@ -293,122 +159,24 @@ async fn main() -> anyhow::Result<()> {
         }) => {
             check_db |= check_all;
             check_server |= check_all;
-
-            tracing::info!("Checking health...");
-            if check_db {
-                match db_health_check().await {
-                    Ok(_) => {
-                        tracing::info!("Database is healthy.");
-                    }
-                    Err(details) => {
-                        tracing::info!(?details, "Database is not healthy.");
-                        std::process::exit(1);
-                    }
-                };
-            };
-
-            if check_server {
-                let client = reqwest::Client::new();
-                let response = client
-                    .get(format!("http://localhost:{}/health", CONFIG.listen_port))
-                    .send()
-                    .await?;
-                let status = response.status();
-                if !status.is_success() {
-                    tracing::info!("Server is not healthy: StatusCode: '{}'", status);
-                    std::process::exit(1);
-                }
-                let body = response.json::<HealthState>().await?;
-                // Fail with an error if the server is not healthy
-                if !matches!(body.health, HealthStatus::Healthy) {
-                    tracing::info!(?body, "Server is not healthy: StatusCode: '{}'", status,);
-                    std::process::exit(1);
-                } else {
-                    tracing::info!("Server is healthy.");
-                }
-            }
+            healthcheck::health(check_db, check_server).await?;
         }
         Some(Commands::Version {}) => {
             println!("{}", env!("CARGO_PKG_VERSION"));
         }
-        None => {
-            // Error out if no subcommand is provided.
-            eprintln!("No subcommand provided. Use --help for more information.");
-        }
-
         Some(Commands::ManagementOpenapi {}) => {
             use utoipa::OpenApi;
             println!("{}", ManagementApiDoc::openapi().to_yaml()?)
+        }
+        None => {
+            // Error out if no subcommand is provided.
+            eprintln!("No subcommand provided. Use --help for more information.");
         }
     }
 
     Ok(())
 }
 
-async fn db_health_check() -> anyhow::Result<()> {
-    let reader = get_reader_pool(
-        CONFIG
-            .to_pool_opts()
-            .acquire_timeout(std::time::Duration::from_secs(1)),
-    )
-    .await
-    .with_context(|| "Read pool failed.")?;
-    let writer = get_writer_pool(
-        CONFIG
-            .to_pool_opts()
-            .acquire_timeout(std::time::Duration::from_secs(1)),
-    )
-    .await
-    .with_context(|| "Write pool failed.")?;
-
-    let db = ReadWrite::from_pools(reader.clone(), writer.clone());
-    db.update_health().await;
-    db.health().await;
-    let mut db_healthy = true;
-
-    for h in db.health().await {
-        tracing::info!("{:?}", h);
-        db_healthy = db_healthy && matches!(h.status(), HealthStatus::Healthy);
-    }
-    if db_healthy {
-        Ok(())
-    } else {
-        Err(anyhow::anyhow!("Database is not healthy."))
-    }
-}
-
 fn print_info() {
     println!("Iceberg Catalog Version: {}", env!("CARGO_PKG_VERSION"));
-}
-
-async fn build_nats_client(nat_addr: &Url) -> Result<NatsBackend, Error> {
-    tracing::info!("Running with nats publisher, connecting to: {nat_addr}");
-    let builder = async_nats::ConnectOptions::new();
-
-    let builder = if let Some(file) = &CONFIG.nats_creds_file {
-        builder.credentials_file(file).await?
-    } else {
-        builder
-    };
-
-    let builder = if let (Some(user), Some(pw)) = (&CONFIG.nats_user, &CONFIG.nats_password) {
-        builder.user_and_password(user.clone(), pw.clone())
-    } else {
-        builder
-    };
-
-    let builder = if let Some(token) = &CONFIG.nats_token {
-        builder.token(token.clone())
-    } else {
-        builder
-    };
-
-    let nats_publisher = NatsBackend {
-        client: builder.connect(nat_addr.to_string()).await?,
-        topic: CONFIG
-            .nats_topic
-            .clone()
-            .ok_or(anyhow::anyhow!("Missing nats topic."))?,
-    };
-    Ok(nats_publisher)
 }
---
+++ crates/iceberg-catalog-bin/src/wait_for_db.rs
@@ -0,0 +1,60 @@
+pub(crate) async fn wait_for_db(
+    check_migrations: bool,
+    retries: u32,
+    backoff: u64,
+    check_db: bool,
+) -> anyhow::Result<()> {
+    if check_db {
+        let mut counter = 0;
+
+        loop {
+            let Err(details) = db_health_check().await else {
+                tracing::info!("Database is healthy.");
+                break;
+            };
+            tracing::info!(?details,
+                        "DB not up yet, sleeping for {backoff}s before next retry. Retry: {counter}/{retries}",
+                    );
+            counter += 1;
+            if counter == retries {
+                tracing::error!("Ran out of retries while waiting for db to come up.");
+                anyhow::bail!("Ran out of retries while waiting for db to come up.");
+            }
+            tokio::time::sleep(std::time::Duration::from_secs(backoff)).await;
+        }
+    }
+
+    if check_migrations {
+        let mut counter = 0;
+        loop {
+            let opts = CONFIG
+                .to_pool_opts()
+                .acquire_timeout(std::time::Duration::from_secs(1));
+
+            let read_pool = get_reader_pool(opts).await?;
+            let migrations =
+                iceberg_catalog::implementations::postgres::check_migration_status(&read_pool)
+                    .await;
+            match migrations {
+                Ok(MigrationState::Complete) => {
+                    tracing::info!("Database is up to date with binary.");
+                    break;
+                }
+                unready => {
+                    tracing::info!(?unready, "Database is not up to date with binary.");
+                }
+            }
+
+            counter += 1;
+            if counter > retries {
+                tracing::error!("Ran out of retries while waiting for migrations.");
+                anyhow::bail!("Ran out of retries while waiting for migrations.");
+            }
+            tracing::info!(
+                        "DB not up to date with binary yet, sleeping for {backoff}s before next retry. Retry: {counter}/{retries}",
+                    );
+            tokio::time::sleep(std::time::Duration::from_secs(backoff)).await;
+        }
+    }
+    Ok(())
+}
