e57b7b8d150323be718f4820b37829959741e481
Tobias Puetz <tobias@hansetag.com>
2024-07-25 18:53:05 +0200

    increase timeouts

--- tests/init_vault.sh
+++ tests/init_vault.sh
@@ -6,6 +6,7 @@ vault auth  enable -address http://hvault:8200  userpass
 echo "path \"secret/*\" { capabilities = [\"create\", \"read\", \"update\", \"delete\", \"list\"] }" > /tmp/app.hcl
 vault policy write -address http://hvault:8200 app /tmp/app.hcl
 vault write -address http://hvault:8200 auth/userpass/users/test password=test policies=app
+vault status -address http://hvault:8200
 # grant access to the secrets kv store to test user
 
 
