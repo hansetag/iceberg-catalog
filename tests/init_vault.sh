cd111a985e5dccf6f42cea564c4452783f4efb5c
Tobias Puetz <tobias@hansetag.com>
2024-07-25 18:36:20 +0200

    fix

--- tests/init_vault.sh
+++ tests/init_vault.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -eux
 
-vault login -address http://hvault:1234 myroot
-vault auth  enable -address http://hvault:1234  userpass
+vault login -address http://hvault:8200 myroot
+vault auth  enable -address http://hvault:8200  userpass
 echo "path \"secret/*\" { capabilities = [\"create\", \"read\", \"update\", \"delete\", \"list\"] }" > /tmp/app.hcl
-vault policy write -address http://hvault:1234 app /tmp/app.hcl
-vault write -address http://hvault:1234 auth/userpass/users/test password=test policies=app
+vault policy write -address http://hvault:8200 app /tmp/app.hcl
+vault write -address http://hvault:8200 auth/userpass/users/test password=test policies=app
 # grant access to the secrets kv store to test user
 
 
