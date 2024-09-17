1433298814b75f8ede67bf516d2cd2a879b8636f
Tobias Puetz <tobias@hansetag.com>
2024-09-17 09:24:14 +0200

    fix sql

--- crates/iceberg-catalog/migrations/20240911094637_unique_tabular_location.sql
+++ crates/iceberg-catalog/migrations/20240911094637_unique_tabular_location.sql
@@ -1,5 +1,5 @@
 -- the old text_pattern_ops index was not useful for our only query which used something like:
 -- select 1 from tabular where '123' like location || '%';
 -- so we drop it and create a new btree index instead that we'll use for exact match queries
-drop index if exists tabular_namespace_id_location_idx;
-create index tabular_location_idx on tabular (namespace_id, location);
+drop index if exists tabular_location_idx;
+create index tabular_namespace_id_location_idx on tabular (namespace_id, location);
