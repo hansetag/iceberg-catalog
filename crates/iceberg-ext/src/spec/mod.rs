e5c1c77fced957cd6703e1ae6ec77e151414a63e
Christian <christian@hansetag.com>
2024-10-02 22:55:31 +0200

    feat: New TableMetadataBuilder with: ID Reassignments, Metadata expiry, safe binding... (#387)

--- crates/iceberg-ext/src/spec/mod.rs
+++ crates/iceberg-ext/src/spec/mod.rs
@@ -2,9 +2,6 @@
 
 pub use iceberg::spec::{
     NullOrder, PartitionSpec, Schema, Snapshot, SortDirection, SortField, SortOrder, TableMetadata,
-    UnboundPartitionField, UnboundPartitionSpec, ViewMetadata, ViewVersion,
+    TableMetadataBuildResult, TableMetadataBuilder, UnboundPartitionField, UnboundPartitionSpec,
+    ViewMetadata, ViewVersion,
 };
-
-mod partition_binder;
-mod table_metadata;
-pub use table_metadata::TableMetadataAggregate;
